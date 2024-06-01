9a3739d5092acfcb5bed0f6067be7335d89267b7
infalliblePirate <palvenko.kateryna@lll.kpi.ua>
2024-06-01 16:40:18 +0300

    Revert "Updated .gitignore."
    This reverts commit 4ebbf89a0b44a5a014432ed2f68ab21184a5b4be, reversing
    changes made to 732ca457d2ec3da4148483f4d1f74f160e4d8422.

--- Deimos/src/Deimos/Renderer/RenderCommand.cpp
+++
@@ -1,8 +0,0 @@
-#include "dmpch.h"
-#include "RenderCommand.h"
-
-#include "Platform/OpenGL/OpenGLRendererAPI.h"
-
-namespace Deimos {
-    RendererAPI* RenderCommand::s_rendererAPI = new OpenGLRendererAPI;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Deimos
-Deimos Engine
