732ca457d2ec3da4148483f4d1f74f160e4d8422
infalliblePirate <palvenko.kateryna@lll.kpi.ua>
2024-06-01 16:00:45 +0300

    Updated .gitignore.

---
+++ CMakeFiles/Sandbox.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pirate/dev/Deimos2/Sandbox/src/SandboxApp.cpp" "Sandbox/CMakeFiles/Sandbox.dir/src/SandboxApp.cpp.o" "gcc" "Sandbox/CMakeFiles/Sandbox.dir/src/SandboxApp.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pirate/dev/Deimos2/build/Deimos/CMakeFiles/Deimos.dir/DependInfo.cmake"
+  "/home/pirate/dev/Deimos2/build/Deimos/vendor/spdlog/CMakeFiles/spdlog.dir/DependInfo.cmake"
+  "/home/pirate/dev/Deimos2/build/Deimos/CMakeFiles/imgui.dir/DependInfo.cmake"
+  "/home/pirate/dev/Deimos2/build/Deimos/vendor/GLFW/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/pirate/dev/Deimos2/build/Deimos/vendor/glm/glm/CMakeFiles/glm.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
