45e9bef236d893045c4ba58bd3a42f2acadfdfc1
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2024-07-20 20:49:37 +0300

    Anchor circles in a chain-like manner

--- Sandbox/src/ProceduralAnimation/MainLayer.cpp
+++ Sandbox/src/ProceduralAnimation/MainLayer.cpp
@@ -12,6 +12,7 @@ void MainLayer::onAttach() {
     DM_PROFILE_FUNCTION();
 
     Deimos::Renderer2D::init();
+    init();
 }
 
 void MainLayer::onDetach() {
@@ -35,8 +36,12 @@ void MainLayer::onUpdate(Timestep timestep) {
     {
         DM_PROFILE_SCOPE("Renderer Draw");
         Renderer2D::beginScene(m_cameraController.getCamera());
-        Renderer2D::drawCircle(m_circlePos, 0.1f, 6, { 107.f / 255, 142.f / 255, 35.f / 255, 1.f });
-        Renderer2D::drawCircle({ 1280 / 720.f, 0.f, 0.3 }, 0.1f, 6, { 92.f / 255, 60.f / 255, 35.f / 255, 1.f });
+
+        for (size_t i = 0; i < m_numJoints; ++i) {
+            Renderer2D::drawCircle(m_spineJoints[i].position, m_spineJoints[i].radius, 32, m_bodyColor);
+        }
+        
+
         Renderer2D::endScene();
     }
 }
@@ -52,11 +57,37 @@ void MainLayer::onImGuiRender() {
 
 }
 
+void MainLayer::init() {
+    // Init the size of body parts
+    m_spineJoints[0].radius = 0.1;
+    m_spineJoints[1].radius = 0.12;
+    m_spineJoints[2].radius = 0.13;
+    m_spineJoints[3].radius = 0.122;
+    m_spineJoints[4].radius = 0.113;
+    m_spineJoints[5].radius = 0.09;
+    m_spineJoints[6].radius = 0.075;
+    m_spineJoints[7].radius = 0.055;
+    m_spineJoints[8].radius = 0.047;
+    m_spineJoints[9].radius = 0.027;
+}
+
+void MainLayer::updateJointsPos()
+{
+    m_spineJoints[0].position = m_headPos;
+    for (size_t i = 1; i < m_numJoints; ++i) {
+        m_spineJoints[i].position = attachToBody(m_spineJoints[i - 1].position, m_spineJoints[i].position, m_startDistance + (i * 0.002));
+    }
+}
+
+glm::vec3 MainLayer::attachToBody(glm::vec3 &achnor, glm::vec3 &point, float distance) {
+    return glm::normalize(point - achnor) * distance + achnor; // obtain normalized vector * distance and translate it to anchor position
+}
+
 bool MainLayer::onMouseMovedEvent(MouseMovedEvent &e) {
     auto& window = Application::get().getWindow();
-    std::cout << "E.GETX()" << e.getX() << std::endl;
-    std::cout << "2.f * e.getx() / window.getWidth() - 1:" << (2.f * e.getX() / window.getWidth()) - 1.f << std::endl;
-    // convert to camera coordinates: x from -window.getWidth() / window.getHeight() to window.getWidth() / window.getHeight()
-    m_circlePos = { ((2.f * e.getX() / window.getWidth()) - 1.f) * window.getWidth() / window.getHeight(), 1.f  - (2.f * e.getY() / window.getHeight()), 0.f};
+    // convert to camera coordinates: x from -window.getWidth() / window.getHeight() to window.getWidth() / window.getHeight(), y from -1 to 1
+    m_headPos = { ((2.f * e.getX() / window.getWidth()) - 1.f) * window.getWidth() / window.getHeight(), 1.f  - (2.f * e.getY() / window.getHeight()), 0.f};        
+    updateJointsPos(); // recalculate position 
+
     return false;
 }
--- Sandbox/src/ProceduralAnimation/MainLayer.h
+++ Sandbox/src/ProceduralAnimation/MainLayer.h
@@ -16,8 +16,22 @@ public:
 
     virtual void onImGuiRender() override;
 private:
+    void init();
+    void updateJointsPos();
+    glm::vec3 attachToBody(glm::vec3& achnor, glm::vec3& point, float distance);
     bool onMouseMovedEvent(Deimos::MouseMovedEvent &e);
 private:
     Deimos::OrthographicCameraController m_cameraController;
-    glm::vec3 m_circlePos{0.f};
+
+    glm::vec4 m_bodyColor = {1/60.f, 1/120.f, 1/160.f, 1.f};
+    struct BodyPart {
+        glm::vec3 position;
+        float radius;
+    };
+    const static int m_numJoints = 10;
+
+    glm::vec3 m_headPos{0.f};
+    glm::vec3 m_pointPos{1.f, 0.5f, 0.f};
+    BodyPart m_spineJoints[m_numJoints]{};
+    float m_startDistance = 0.1f;
 };
